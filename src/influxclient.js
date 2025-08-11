98ce60dff8bba605298f19f292e21a9d238cd848
unknown <jeanpierremarktine1612@gmail.com>
2025-08-11 19:46:08 +0000

    Commit 11/08

--- src/influxclient.js
+++ src/influxclient.js
@@ -1,7 +1,7 @@
 const { InfluxDB } = require('@influxdata/influxdb-client');
 
 const url = process.env.INFLUX_URL || 'http://localhost:8086';
-const token = process.env.INFLUXDB_TOKEN;
+const token = process.env.INFLUX_TOKEN
 const org = process.env.INFLUX_ORG || 'JPCDI';
 
 const influxDB = new InfluxDB({ url, token });
